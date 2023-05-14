834599ede4968787b69c71da425b2bc8ec387a61
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2023-05-14 14:55:15 -0400

    Added department, department show/delete, department add/edit components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    AddEditDepComponent
   ],
   imports: [
     BrowserModule,
